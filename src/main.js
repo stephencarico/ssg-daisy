337f56e3e3a958a4f99558cb43d695ff2e2a6db7
Stephen Carico <32233894+stephencarico@users.noreply.github.com>
2022-08-12 18:31:26 -0600

    establish ssg

--- src/main.js
+++ src/main.js
@@ -1,5 +1,16 @@
-import { createApp } from 'vue'
-import './style.css'
+import { ViteSSG } from 'vite-ssg'
 import App from './App.vue'
+import routes from './routes'
+import './style.css'
 
-createApp(App).mount('#app')
+// `export const createApp` is required instead of the original `createApp(App).mount('#app')`
+export const createApp = ViteSSG(
+  // the root component
+  App,
+  // vue-router options
+  { routes },
+  // function to have custom setups
+  ({ app, router, routes, isClient, initialState }) => {
+    // install plugins etc.
+  },
+)
